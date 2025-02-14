20b43ceb9b55192d58d87515c4964838dd6a9d5b
mantarias <32459446+mantarias@users.noreply.github.com>
2025-02-14 14:52:03 +0100

    i suck

--- main.ts
+++ main.ts
@@ -185,7 +185,7 @@ function onMessage(event: string, user: User) {
   let data = JSON.parse(event);
   user.changes = data;
   console.log(data);
-  for (let outerIndex = 0; outerIndex < data.length; outerIndex-- ){
+  for (let outerIndex = 0; outerIndex < data.length; outerIndex++ ){
     let index = 0;
     console.log(data[outerIndex].ops);
     data[outerIndex].ops.forEach((el) => {
@@ -194,7 +194,7 @@ function onMessage(event: string, user: User) {
         console.log("updated index to " + index);
       } else if (el.insert != undefined) {
         console.log("inserted");
-        for(let i = el.insert.length-1; i >= 0; i++)
+        for(let i = el.insert.length-1; i >= 0; i--)
         {
           user.list.insert(index, el.insert[i]);
         }
